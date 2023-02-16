4d7064a6ecc8c87a8479fb98cb65175d0807eb8a
Derek Hockett <derhockett@gmail.com>
2023-02-16 02:24:31 -0600

    Print help if called with no args

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
-        1..=4 => process_command(&args[1], &args.split_at(2).1.to_vec()),
+        1 => print_help(),
+        2..=4 => process_command(&args[1], &args.split_at(2).1.to_vec()),
         _ => println!("Too many arguments")
     }
 }
@@ -53,4 +54,14 @@ fn delete(arguments: &Vec<String>) {
         0 => println!("'Delete' expects an argument (name of sticky note to delete)."),
         _ => db_operations::delete_sticky_note(&arguments[0])
     }
+}
+
+fn print_help() {
+    println!("Supported arguments for 'sn':
+
+    list                | Lists all sticky note names and a brief preview of their contents.
+    read <name>         | Prints the full sticky note with the given name.
+    write <name> <text> | Writes a new sticky note with the given name - no-ops if a note with the given the name already exists.
+    edit <name> <text>  | Edits the sticky note with the given name - writes a new one if a note with the given the name couldn't be found.
+    delete <name>       | Deletes the note with the given name.")
 }
